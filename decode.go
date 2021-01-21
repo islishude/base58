5c9bceae2c05b4e2c3fc5a5c8778eb43d3d974f8
islishude <islishude@gmail.com>
2021-01-21 22:51:39 +0800

    supports monero

--- decode.go
+++ decode.go
@@ -12,7 +12,7 @@ func Decode(input string) (res []byte, err error) {
 	isBreak := false
 	for i := 0; i < len(input); i++ {
 		char := input[i]
-		weight := strings.IndexByte(table, char)
+		weight := strings.IndexByte(_table, char)
 		if weight == -1 {
 			return nil, errors.New("Invalid character in input string")
 		}
@@ -30,5 +30,47 @@ func Decode(input string) (res []byte, err error) {
 		}
 	}
 	res = append(res, result.Bytes()...)
+	return
+}
+
+var _lookup = map[rune]int64{
+	'1': 0, '2': 1, '3': 2, '4': 3, '5': 4, '6': 5, '7': 6, '8': 7, '9': 8, 'A': 9,
+	'B': 10, 'C': 11, 'D': 12, 'E': 13, 'F': 14, 'G': 15, 'H': 16, 'J': 17, 'K': 18,
+	'L': 19, 'M': 20, 'N': 21, 'P': 22, 'Q': 23, 'R': 24, 'S': 25, 'T': 26, 'U': 27,
+	'V': 28, 'W': 29, 'X': 30, 'Y': 31, 'Z': 32, 'a': 33, 'b': 34, 'c': 35, 'd': 36,
+	'e': 37, 'f': 38, 'g': 39, 'h': 40, 'i': 41, 'j': 42, 'k': 43, 'm': 44, 'n': 45,
+	'o': 46, 'p': 47, 'q': 48, 'r': 49, 's': 50, 't': 51, 'u': 52, 'v': 53, 'w': 54,
+	'x': 55, 'y': 56, 'z': 57,
+}
+
+func decodeChunk(encoded string) []byte {
+	bigResult := big.NewInt(0)
+	currentMultiplier := big.NewInt(1)
+	tmp := new(big.Int)
+
+	for _, v := range encoded {
+		index, valid := _lookup[v]
+		if !valid {
+			return nil // TODO: return error
+		}
+
+		tmp.SetInt64(index)
+		tmp.Mul(currentMultiplier, tmp)
+		bigResult.Add(bigResult, tmp)
+		currentMultiplier.Mul(currentMultiplier, base)
+	}
+
+	return bigResult.Bytes()
+}
+
+func DecodeMonero(input string) (result []byte) {
+	length := len(input)
+	rounds := length / 11
+	for i := 0; i < rounds; i++ {
+		result = append(result, decodeChunk(input[i*11:(i+1)*11])...)
+	}
+	if length%11 > 0 {
+		result = append(result, decodeChunk(input[rounds*11:])...)
+	}
 	return
 }
--- encode.go
+++ encode.go
@@ -2,12 +2,13 @@ package base58
 
 import (
 	"math/big"
+	"strings"
 )
-
-const table = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 
 var base = big.NewInt(58)
 var zero = new(big.Int)
+
+const _table = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 
 // Encode encodes bytes to base58 string
 func Encode(src []byte) (res string) {
@@ -15,7 +16,7 @@ func Encode(src []byte) (res string) {
 	mod := new(big.Int)
 	for x.Cmp(zero) > 0 {
 		x.QuoRem(x, base, mod)
-		res = string(table[mod.Int64()]) + res
+		res = string(_table[mod.Int64()]) + res
 	}
 
 	for _, char := range src {
@@ -24,6 +25,33 @@ func Encode(src []byte) (res string) {
 		} else {
 			break
 		}
+	}
+	return
+}
+
+func encodeChunk(raw []byte, padding int) (result string) {
+	remainder := new(big.Int)
+	remainder.SetBytes(raw)
+	bigZero := new(big.Int)
+	for remainder.Cmp(bigZero) > 0 {
+		current := new(big.Int)
+		remainder.DivMod(remainder, base, current)
+		result = string(_table[current.Int64()]) + result
+	}
+	if len(result) < padding {
+		result = strings.Repeat("1", (padding-len(result))) + result
+	}
+	return
+}
+
+func EncodeMonero(src []byte) (result string) {
+	length := len(src)
+	rounds := length / 8
+	for i := 0; i < rounds; i++ {
+		result += encodeChunk(src[i*8:(i+1)*8], 11)
+	}
+	if length%8 > 0 {
+		result += encodeChunk(src[rounds*8:], 7)
 	}
 	return
 }
